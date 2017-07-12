71bc43c910d5c2d99e36e9365df6c74e7a5684fa
Sowmya Logaswamy, Mick Sexton and saul artiga <sowmya@>
2017-07-12 15:04:40 -0700

    add home page GET route with Lists for Artists andAlbums and button for new review submission

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -5,5 +7,16 @@ import static spark.Spark.*;
 public class App {
 
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("artists", Artist.all());
+      model.put("albums", Album.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
