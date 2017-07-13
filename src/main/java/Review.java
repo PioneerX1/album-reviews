fb99c0752403c03bf987f0a91f5f6a3e5f33ab06
Sowmya Logaswamy, Mick Sexton and saul artiga <sowmya@>
2017-07-13 09:55:37 -0700

    finish Review class and add tests for reviews

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -15,4 +15,92 @@ public class Review{
     this.albumId = albumId;
   }
 
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO reviews(name, comment, rating, albumId) VALUES (:name, :comment, :rating, :albumId);";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("comment", this.comment)
+      .addParameter("rating", this.rating)
+      .addParameter("albumId", this.albumId)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static List<Review> all(){
+    // String sql = "SELECT id, name, comment, rating, albumId FROM reviews";
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM reviews;";
+      return con.createQuery(sql).executeAndFetch(Review.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherReview){
+    if(!(otherReview instanceof Review)){
+      return false;
+    } else {
+      Review newReview = (Review) otherReview;
+      return this.getId() == newReview.getId() &&
+      this.getName().equals(newReview.getName()) &&
+      this.getComment().equals(newReview.getComment()) &&
+      this.getRating() == newReview.getRating() &&
+      this.getAlbumId() == newReview.getAlbumId();
+    }
+  }
+
+  public static Review find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM reviews WHERE id=:id;";
+      Review review = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Review.class);
+        return review;
+    }
+  }
+
+  public void update(String name, String comment, int rating) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE reviews SET name = :name, comment = :comment, rating = :rating WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("comment", comment)
+        .addParameter("rating", rating)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM reviews WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+
+
+  public String getName() {
+    return name;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getAlbumId() {
+    return albumId;
+  }
+
 }
--- src/test/java/ReviewTest.java
+++ src/test/java/ReviewTest.java
@@ -13,4 +13,73 @@ public class ReviewTest{
     assertEquals(true, testReview1 instanceof Review);
   }
 
+  @Test
+  public void getName_retrievesName_red() {
+    Review testReview1 = new Review("red", "thought it sucked", 1, 3);
+    assertEquals("red", testReview1.getName());
+  }
+
+  @Test
+  public void getComment_retrievesComment_mindBlowing() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    assertEquals("mind blowing", testReview1.getComment());
+  }
+
+  @Test
+  public void getRating_retrievesRating_1() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    assertEquals(1, testReview1.getRating());
+  }
+
+  @Test
+  public void getAlbumId_retrievesAlbumId_1() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    assertEquals(1, testReview1.getAlbumId());
+  }
+
+  @Test
+  public void save_createsId_true() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    testReview1.save();
+    assertEquals(true, testReview1.getId() > 0);
+  }
+
+  @Test
+  public void all_retrievesSavedReviews() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    testReview1.save();
+    Review testReview2 = new Review("yellow", "its was kk", 2, 2);
+    testReview2.save();
+    assertEquals(true, Review.all().get(0).equals(testReview1));
+    assertEquals(true, Review.all().get(1).equals(testReview2));
+  }
+
+  @Test
+  public void find_retrievesReviewWithSameId_testReview2() {
+    Review testReview1 = new Review("blue", "mind blowing", 1, 1);
+    testReview1.save();
+    Review testReview2 = new Review("yellow", "its was kk", 2, 2);
+    testReview2.save();
+    assertEquals(Review.find(testReview2.getId()), testReview2);
+  }
+
+  @Test
+  public void update_updatesReviewRating_2() {
+    Review testReview1 = new Review("blue", "mind blowing", 5, 1);
+    testReview1.save();
+    testReview1.update("red", "straight fire", 2);
+    assertEquals("red", Review.find(testReview1.getId()).getName());
+    assertEquals("straight fire", Review.find(testReview1.getId()).getComment());
+    assertEquals(2, Review.find(testReview1.getId()).getRating());
+  }
+
+  @Test
+  public void delete_deletesReviewEntryFromDB_null() {
+    Review testReview1 = new Review("blue", "mind blowing", 5, 1);
+    testReview1.save();
+    int testReviewId = testReview1.getId();
+    testReview1.delete();
+    assertEquals(null, Review.find(testReviewId));
+  }
+
 }
