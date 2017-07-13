0a47e95c0f36cccd9c993465e61f383e37106a0f
Sowmya Logaswamy, Mick Sexton and saul artiga <mick@>
2017-07-13 16:44:47 -0700

    add some styling and work on form on home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -63,5 +63,36 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/new-review", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-review-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/success", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      //create Artist object
+      String userArtist = request.queryParams("user-artist");
+      Artist artist = new Artist(userArtist);
+      artist.save();
+      //create Album object
+      String userAlbumName = request.queryParams("user-album-name");
+      int userAlbumYear = Integer.parseInt(request.queryParams("user-album-year"));
+      String userAlbumGenre = request.queryParams("user-album-genre");
+      Album album = new Album(userAlbumName, userAlbumYear, userAlbumGenre, artist.getId());
+      album.save();
+      //create Review object
+      String reviewName = request.queryParams("name");
+      String reviewComment = request.queryParams("comment");
+      int reviewRating = Integer.parseInt(request.queryParams("rating"));
+      Review newReview = new Review(reviewName, reviewComment, reviewRating, album.getId());
+      newReview.save();
+      model.put("artist", artist);
+      model.put("album", album);
+      model.put("reviews", Review.findAlbumId(album.getId()));
+      model.put("template", "templates/album.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
